cb442aa1856165bc4d7ff9c80a047b48635b793c
[React-Hooks] <[ruslanprivozin277@gmail.com]>
2023-12-21 10:26:36 +0300

    control saving

--- workingTest/BlockContact.js
+++ workingTest/BlockContact.js
@@ -65,9 +65,6 @@ async function testSearchContact(driver) {
     await driver.get(
         "https://auth.radist.online/auth/realms/radist/protocol/openid-connect/auth?client_id=web-ui-test&redirect_uri=https%3A%2F%2Fapp-beta.int.radist.online%2Fcompanies%2F1%2Fchats%2F643%2F452367&state=a2095ef3-ee00-4893-8bf6-a596644a6bfa&response_mode=fragment&response_type=code&scope=openid&nonce=d8ba6edb-4e15-4869-850e-17cec5778cd6"
     );
-    await driver.findElement(By.css("#username")).sendKeys(`${LOGIN}`);
-    await driver.findElement(By.css("#password")).sendKeys(`${PASSWORD}`, Key.ENTER);
-    await driver.sleep(1000);
     await driver.wait(until.elementLocated(By.css("#chats")));
     await driver.sleep(1000);
     await driver.findElement(By.css('#contacts')).click();
@@ -96,9 +93,6 @@ async function testSortingContact(driver) {
     await driver.get(
         "https://auth.radist.online/auth/realms/radist/protocol/openid-connect/auth?client_id=web-ui-test&redirect_uri=https%3A%2F%2Fapp-beta.int.radist.online%2Fcompanies%2F1%2Fchats%2F643%2F452367&state=a2095ef3-ee00-4893-8bf6-a596644a6bfa&response_mode=fragment&response_type=code&scope=openid&nonce=d8ba6edb-4e15-4869-850e-17cec5778cd6"
     );
-    await driver.findElement(By.css("#username")).sendKeys(`${LOGIN}`);
-    await driver.findElement(By.css("#password")).sendKeys(`${PASSWORD}`, Key.ENTER);
-    await driver.sleep(1000);
     await driver.wait(until.elementLocated(By.css("#chats")));
     await driver.findElement(By.css('#contacts')).click();
     await driver.sleep(2000);
